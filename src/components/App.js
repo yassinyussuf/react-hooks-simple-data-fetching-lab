00707ec0724a8cb3e8803113fed6e5386638f2c4
yassinyussuf <yassinyussuf414@gmail.com>
2024-05-15 11:45:35 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [image, setImage] = useState(null);
+  
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => setImage(data.message))
+        .catch((error) => console.error("Error fetching dog image:", error));
+    }, []);
+  
+    if (!image) {
+      return <p>Loading...</p>;
+    }
+  
+    return <img alt="A Random Dog" src={image} />;
+  }
+  
+  export default App;
